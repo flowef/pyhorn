7b51404d9c278f752dbd4e94565d6c7736808754
Stephan Chang <stephan.chang@flowef.com>
2019-08-09 17:22:12 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+short_description = "An unofficial Bullhorn REST API client written in Python."
+
+setuptools.setup(
+    name="pyhorn-flow",
+    version="1.0.0",
+    author="Stephan Chang",
+    author_email="stephan.chang@flowef.com",
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flowef/pyhorn/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
